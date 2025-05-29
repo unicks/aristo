bd67dfe245392f5a536bdd1c4f7d84d3cc98ed17
MaorKertsman <107365915+MaorKertsman@users.noreply.github.com>
2025-05-29 18:51:00 +0300

    Update system.py

--- system.py
+++ system.py
@@ -1,6 +1,6 @@
 '''
-    User chooses assignment in backend  --->    Get assigments from moodle     ---> 
+    User chooses assignment in front    --->    Get assigments from moodle     ---> 
     Let nick choose N representatives   --->    Send representatives to front  --->
     Get context from front              --->    Get grades for all submissions --->
-    Upload submissions to moodle        --->    Win hackaton
+    Upload submissions to moodle        --->    Win hackaton                   ---> 10k straight to the bank account
 '''
