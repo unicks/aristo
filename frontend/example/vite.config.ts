bf4b9ff76e55b60cdce44cd0a8673432ec8c0381
unicks <nickbhe@gmail.com>
2025-05-30 09:27:09 +0300

    update config for vercel amen

--- frontend/example/vite.config.ts
+++ frontend/example/vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/react-pdf-highlighter/",
   build: {
     outDir: "dist",
   },
